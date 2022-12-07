d00a0bdced13eec7f54c5112cc4fb2ce0a7a9cdc
Yethu <yethukichu55@gmail.com>
2022-12-07 09:16:52 +0530

    testing the css  for wrapper class

--- netflix-ui/src/components/list/List.js
+++ netflix-ui/src/components/list/List.js
@@ -1,16 +1,39 @@
 import React from 'react'
 import "./List.css"
 import ListItem from '../ListItem/ListItem'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 function List() {
   return (
     <div className='list'>
-      <div className='items'>
-        <ListItem></ListItem>
-        <ListItem></ListItem>
-        <ListItem></ListItem>
+        <span className='listTitle'>Continue to watch</span>
+        <div className='wrapper'>
+        <div className='items'>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+          <ListItem></ListItem>
+        </div>
+         <ArrowBackIosIcon sx={{color:"white",backgroundColor:"red"}} className='sliderArrow-left'/>
+        <ArrowForwardIosIcon sx={{color:"white",backgroundColor:"red"}} className='sliderArrow-right'/>
       </div>
-     
     </div>
   )
 }
